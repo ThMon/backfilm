24bad2c8d09c09c24b5d6d6f28bc44f9edcf3e13
Thibaut <thibaut.monesma@gmail.com>
2018-09-28 00:03:03 +0200

    add date and geoloc

--- js/utilities.js
+++ js/utilities.js
@@ -1,3 +1,6 @@
+// recupération paramètre
+
+
 function extractUrlParams () {
 	var t = location.search.substring(1).split('&');
 
@@ -12,27 +15,72 @@ function extractUrlParams () {
 
 var params = extractUrlParams();
 
-//console.log(params);
+// geoloc
 
-/*
-var date = new Date();
+if(navigator.geolocation) {
+  console.log('la');
+} else {
+  // Pas de support, proposer une alternative ?
+  console.log('pas la');
+}
 
-console.log(date.getTime());
+function position(pos) {
+	console.log('latitude', pos.coords.latitude);
+	console.log('longitude', pos.coords.longitude);
 
-console.log(date.getTime()+86400);
+}
+
+navigator.geolocation.getCurrentPosition(position);
 
 
-var date2 = convertirTimestamp(date.getTime());
-console.log(date2);
+// récupération date
 
-var jours = new Array("dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi");
-*/
+function automatiseCalendar() {
+	var date = new Date();
 
-console.log($('.days li')[0]);
+	var days = ['Dim','Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam', 'Dim'];
+
+
+	var tab = [];
+
+	tab.push({
+		id: 1,
+		day: days[date.getDay()],
+		num: date.getDate(),
+		full: date
+	});
+
+	$('#date-1 .s1').text(days[date.getDay()]);
+	$('#date-1 .s2').text(date.getDate());
+	$('#date-1').attr('data-date', date.getFullYear()+'/'+(date.getMonth()+1)+'/'+date.getDate());
+
+	for (var i = 2; i < 8; i++) {
+		date.setDate(date.getDate() + 1);
+		$('#date-'+i+' .s1').text(days[date.getDay()]);
+		$('#date-'+i+' .s2').text(date.getDate());
+		$('#date-'+i).attr('data-date', date.getFullYear()+'/'+(date.getMonth()+1)+'/'+date.getDate());
+		
+		tab.push({
+			id: i,
+			day: days[date.getDay()],
+			num: date.getDate(),
+			full: date
+		});
+	}
+
+console.log(tab);
+
+}
+
+automatiseCalendar()
+
+
+// select date
+// gestion requete en fonction de la localisation et date 
 
 function changeSelectedDay() {
 	
-		$('.days li').removeClass('blue');
+	$('.days li').removeClass('blue');
 	
 
 	$(this).addClass('blue')
